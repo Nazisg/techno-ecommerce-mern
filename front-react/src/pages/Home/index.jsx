bdaa21f7a8b773b685618dfe8b9cc470027a52c9
Nazrin Isgandarova <isgnezrin@gmail.com>
2024-10-21 23:02:21 +0400

    create products page

--- front-react/src/pages/Home/index.jsx
+++ front-react/src/pages/Home/index.jsx
@@ -4,13 +4,13 @@ import BlogPosts from './sections/BlogPosts';
 import CategorySlider from './sections/CategorySlider';
 import DiscountBanner from './sections/DiscountBanner';
 import Feedback from './sections/Feedback';
+import Gallery from './sections/Gallery';
 import LimitedTimeOffer from './sections/LimitedTimeOffer';
 import OfferBanner from './sections/OfferBanner';
 import Patners from './sections/Partners';
 import Products from './sections/Products';
 import BannerSlider from './sections/Slider';
 import TopNewArrivals from './sections/TopNewArrivals';
-import Gallery from './sections/Gallery';
 
 function Home() {
 
@@ -26,7 +26,7 @@ function Home() {
       <Feedback />
       <BlogPosts />
       <Patners />
-      <Gallery/>
+      <Gallery />
     </>
   );
 }
--- front-react/src/pages/Products/index.jsx
+++ front-react/src/pages/Products/index.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import PageHeader from '../../components/PageHeader'
+import ProductCard from '../../components/ProductCard'
+import { LuSettings2 } from "react-icons/lu";
 
 function Products() {
   return (
-    <div>Products</div>
+    <div>
+      <PageHeader route={'/products'} title={'Products'} />
+      <div className='flex gap-4 justify-center py-8 md:py-10 px-6 md:px-8 w-full'>
+        <div className='w-[25%] border border-[#636363]'>
+          <div className='flex items-center gap-2 border-b py-3 px-4 border-b-[#636363]'>
+            <LuSettings2 className='text-2xl' />
+            <p className='text-2xl'>Filter</p>
+          </div>
+        </div>
+        <div className='w-[75%] flex flex-col gap-2'>
+          <div className='border border-[#636363] flex justify-between gap-3 items-center py-3 px-5'>
+            <p>5 products viewed</p>
+            <div className='flex gap-3 items-center'>
+              <p>Sort by:</p>
+              <select name="" id="" className='border border-[#636363] bg-transparent p-2'>
+                <option className='bg-black' defaultValue={"best selling"} value="">best selling</option>
+                <option className='bg-black' value="">A to Z</option>
+                <option className='bg-black' value="">Z to A</option>
+              </select>
+            </div>
+          </div>
+          <div className="grid sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-8 pt-5 md:pt-8">
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
